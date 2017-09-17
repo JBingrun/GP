cafb3a35f27bfcb29629a7cc3f3bca7328276f08
BingrunJ <nickb771223@gmail.com>
2017-09-17 12:55:05 +0800

    add Col

---
+++ Script/Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var viewport_scale
+
+func _ready():
+	#var viewport = get_node("/root/World")
+	#var viewport = get_node("/root").get_children()[1].get_viewport_rect().size
+	#print(viewport.get_name())
+	#print(viewport)
+	pass
---
+++ Script/Level.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var Col_Total = 0
+var Col_collected = 0
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
